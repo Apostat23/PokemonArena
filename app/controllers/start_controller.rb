15df4d93dbf0a227e704933a6799f1ab987e4339
Apostat23 <diego.mota@wizeline.com>
2021-09-25 10:22:54 -0500

    Modified app/controllers/start_controller.rb

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -4,8 +4,15 @@ class StartController < ApplicationController
   before_action :get_pokemon
 
   def get_pokemon
-    params[:url].present? ? @pokemon = Arena::Pokemon.new(url: params[:url]) : @pokemon = Arena::Pokemon.new
-    @data = @pokemon.data; @list = @pokemon.list; @info_tags = @pokemon.info_tags
+    params[:url].present? ? @request = get_data(params[:url]) : @request = get_data
+    @data = @request.data; @list = @request.list; @info_tags = @request.info_tags
+  end
+
+  attr_accessor :data, :list, :info_tags and def get_data url: nil
+    default = 'https://pokeapi.co/api/v2/pokemon' and @data = JSON.parse(RestClient.get(url || default))
+    @list = Hash.new and @data["results"].each {|r| @list.store(r["name"], JSON.parse(RestClient.get(r["url"])))}
+    @info_tags = @list.first.last.keys # <= Check tags when updating methods in RSpec.
+    @info_tags.each {|it| self.class.define_method("get_#{it}") {|pokemon, list| list[pokemon][it]}}
   end
 
   def index
